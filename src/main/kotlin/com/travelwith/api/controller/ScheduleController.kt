8549b0975c2b5d1dd7a1907f1e5e750fc920f60c
이태훈 <thxwelchs@gmail.com>
2019-11-07 18:44:41 +0900

    [UPDATE] prod profile 수정

--- src/main/kotlin/com/travelwith/api/controller/ScheduleController.kt
+++ src/main/kotlin/com/travelwith/api/controller/ScheduleController.kt
@@ -4,8 +4,6 @@ import com.travelwith.api.annotaion.V1ApiController
 import com.travelwith.api.model.Schedule
 import com.travelwith.api.repository.ScheduleRepository
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 
 @V1ApiController
 class ScheduleController(
--- src/main/kotlin/com/travelwith/api/controller/TravelController.kt
+++ src/main/kotlin/com/travelwith/api/controller/TravelController.kt
@@ -3,10 +3,7 @@ package com.travelwith.api.controller
 import com.travelwith.api.annotaion.V1ApiController
 import com.travelwith.api.model.Travel
 import com.travelwith.api.repository.TravelRepository
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 
 @V1ApiController
 class TravelController(

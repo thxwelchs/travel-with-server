c5a72dd89bb164331355e501397dd1d3b1edf26c
이태훈 <thxwelchs@gmail.com>
2019-11-13 18:54:21 +0900

    [UPDATE] RestControllerAdvice ErrorCode Enum 값으로 수정

--- src/main/kotlin/com/travelwith/api/controller/ApiControllerAdvice.kt
+++ src/main/kotlin/com/travelwith/api/controller/ApiControllerAdvice.kt
@@ -2,6 +2,7 @@ package com.travelwith.api.controller
 
 import com.travelwith.api.model.ApiResponse
 import com.travelwith.api.model.ApiResponseError
+import com.travelwith.api.model.ErrorCode
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -18,7 +19,7 @@ class ApiControllerAdvice {
         val apiResponse = ApiResponse().apply {
             httpStatus = httpStatusCode
             message = e.message.toString()
-            error = ApiResponseError("100", e.message.toString())
+            error = ApiResponseError(ErrorCode.INTERNAL_SERVER_ERROR, e.message.toString())
         }
 
         return ResponseEntity(apiResponse, HttpStatus.INTERNAL_SERVER_ERROR)

e99b639340d02ec988becdb144e20f0bfeed46ff
이태훈 <thxwelchs@gmail.com>
2019-11-13 18:46:49 +0900

    [UPDATE] TravelController Swagger 관련 주석

--- src/main/kotlin/com/travelwith/api/controller/TravelController.kt
+++ src/main/kotlin/com/travelwith/api/controller/TravelController.kt
@@ -3,13 +3,17 @@ package com.travelwith.api.controller
 import com.travelwith.api.annotaion.V1ApiController
 import com.travelwith.api.model.Travel
 import com.travelwith.api.repository.TravelRepository
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
 import org.springframework.web.bind.annotation.GetMapping
 
+@Api(description = "Travel(여행) API")
 @V1ApiController
 class TravelController(
         private val travelRepository: TravelRepository
 ) {
 
+    @ApiOperation("모든 여행 조회")
     @GetMapping("/travels")
     fun getTravels(): MutableList<Travel> {
         return travelRepository.findAll()

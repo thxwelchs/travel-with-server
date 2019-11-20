dd43366904c1cf2dae60fadc84a1eb1aaf5e13a0
이태훈 <thxwelchs@gmail.com>
2019-11-20 23:47:11 +0900

    [UPDATE] TravelMember 에서 해당 Member, Travel로 조회하는 메소드 추가

--- src/main/kotlin/com/travelwith/api/repository/TravelMemberRepository.kt
+++ src/main/kotlin/com/travelwith/api/repository/TravelMemberRepository.kt
@@ -1,9 +1,11 @@
 package com.travelwith.api.repository
 
 import com.travelwith.api.model.Member
+import com.travelwith.api.model.Travel
 import com.travelwith.api.model.TravelMember
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface TravelMemberRepository: JpaRepository<TravelMember, Int> {
-    fun findByMember(member: Member)
+    fun findByTravel(travel: Travel): MutableList<TravelMember>
+    fun findByMember(member: Member): MutableList<TravelMember>
 }
